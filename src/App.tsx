e059f0cd0f3a71c3a6252e652be6228ae4769691
rodrigo-v4z <rodrigovazd.oliveira@gmail.com>
2025-07-25 11:45:08 -0300

    Versão Lista de Tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { adicionarTarefa, removerTarefa } from './utils/lista';
 
 function App() {
+  const [lista_tarefas, setLista] = useState([]);
+  const [nova_tarefa, setTarefa] = useState('');
 
+  function handleAdicionar() {
+    if (nova_tarefa.trim() === '') return;
+    setLista(adicionarTarefa(lista_tarefas, nova_tarefa));
+    setTarefa('');
+  }
+
+  function handleRemover(tarefa) {
+    setLista(removerTarefa(lista_tarefas, tarefa));
+  }
 
   return (
     <>
-      
+      <input
+        type="text"
+        value={nova_tarefa}
+        onChange={e => setTarefa(e.target.value)}
+        placeholder="Digite uma tarefa"
+      />
+      <button onClick={handleAdicionar}>Adicionar Tarefa</button>
+
+      <ul>
+        {lista_tarefas.map((tarefa, index) => (
+          <li key={index}>
+            {tarefa}{' '}
+            <button onClick={() => handleRemover(tarefa)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
